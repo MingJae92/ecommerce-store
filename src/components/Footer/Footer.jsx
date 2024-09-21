dd4f7f2c280b51d85d23131eae1e966b628cd807
Ming Chi <wong_mingchi@yahoo.co.uk>
2024-09-21 22:39:26 +0100

    Footer fix to the bottom on the page when scrolling

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -6,15 +6,13 @@ const Footer = () => {
     <Box
       component="footer"
       sx={{
-        position: 'fixed',
-        bottom: 0,
-        left: 0,
         width: '100%',
         minHeight: '7vh',
         display: 'flex',
         alignItems: 'center',
         justifyContent: 'center',
         backgroundColor: '#f5f5f5',
+        marginTop: 'auto', // Pushes the footer to the bottom of the page
       }}
     >
       <Typography variant="body2" color="text.secondary">
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,12 @@
+// Navbar.js
 import React from 'react';
 import { AppBar, Toolbar, Typography, Button } from '@mui/material';
 
 const Navbar = () => {
   return (
-    <AppBar position="fixed" sx={{ width: '100%' }}>
+    <AppBar position="fixed" sx={{ backgroundColor: '#2e4c9a', width: '100%' }}>
       <Toolbar>
-        <Typography variant="h6" sx={{ flexGrow: 1 }}>
+        <Typography variant="h6" sx={{ flexGrow: 1, color: '#fff' }}>
           My Store
         </Typography>
         <Button color="inherit">Login</Button>
