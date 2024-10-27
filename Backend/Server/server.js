71969cdec6eb759f18858b6a8701364f89a937ce
Ming Chi <wong_mingchi@yahoo.co.uk>
2024-10-27 23:20:16 +0000

    Server env file fixed

--- Backend/Server/server.js
+++ Backend/Server/server.js
@@ -2,7 +2,6 @@ import express from "express";
 import connectDB from "../Database/connection.js";
 import dotenv from "dotenv";
 
-
 // Correct syntax for dotenv.config with path option
 dotenv.config({ path: "./Config/.env" });
 
