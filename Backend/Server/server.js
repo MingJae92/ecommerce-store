cd2bc9fa2f550feab8877e9f0e1cec0fcc3f0287
Ming Chi <wong_mingchi@yahoo.co.uk>
2024-10-27 23:26:35 +0000

    BE config folder removed

--- Backend/Server/server.js
+++ Backend/Server/server.js
@@ -3,10 +3,11 @@ import connectDB from "../Database/connection.js";
 import dotenv from "dotenv";
 
 // Correct syntax for dotenv.config with path option
-dotenv.config({ path: "./Config/.env" });
+dotenv.config({ path: "../config/.env" });
 
+const PORT= process.env.SERVER_PORT
 const app = express();
-const PORT = 9000
+
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
