f6ee96d9e4b9d41c58eba3e3873c11c7c0b320a4
Ming Chi <wong_mingchi@yahoo.co.uk>
2024-11-03 22:05:20 +0000

    bycrpt and bcryptjs added

--- Backend/Controllers/signUpController/signUpController.js
+++ Backend/Controllers/signUpController/signUpController.js
@@ -1,19 +1,23 @@
+import bcrypt from "bcryptjs"
 import signUp from "../../Models/signUpModel/signUpSchema.js";
 
 const signUpController = async (req, res) => {
   try {
+    // Hash the password before saving
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+
     const userData = await signUp.create({
       name: req.body.name,
       email: req.body.email,
-      password: req.body.password,
-      confirmpassword: req.body.confirmpassword,
+      password: hashedPassword,  // Store the hashed password
+      confirmpassword: hashedPassword, // Assuming you also want to store this hashed
     });
-    
+
     console.log(JSON.stringify(userData));  // Logging before sending response
     res.status(200).json({ message: "User successfully added!", user: userData });
   } catch (error) {
     res.status(500).json({ message: "Internal server error!", error });
-    console.log({error})
+    console.log({error});
   }
 };
 
